0045c56c6b1c36dbd43a3357add2466828c0f030
Linski <molinski@gideonsp.com>
2022-04-25 14:18:15 -0700

    created updateNodesToAdd and added nodestoadd file

--- serialize.py
+++ serialize.py
@@ -25,10 +25,15 @@ class TreeNode:
 
 
 class Codec:
+ def updateNodesToAdd(self, player: str, question: str):
+   file = open(os.getcwd() + "/nodestoadd.txt", "a")
+   file.append("After question: " + question + " new player: " + player + "\n");
+   file.close()
  def updateTxt(self, fileName:str, data):
    file = open(os.getcwd() + "/" + fileName, "w")
    for i in data:
      file.write(i);
+   file.close()
    return
  def txtToTokens(self, fileName: str):
          file = open(os.getcwd() + "/" + fileName, "r")
