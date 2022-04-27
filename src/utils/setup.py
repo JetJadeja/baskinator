8f9c33bfe6f98e6579edbf7f25469cf0b6231a59
JetJadeja <jetjadeja@gmail.com>
2022-04-26 18:45:23 -0700

    Write csv conversion function

---
+++ src/utils/setup.py
@@ -0,0 +1,45 @@
+from tree import DecisionTree
+import os
+
+file = open(f"{os.getcwd()}/src/data/data.txt", "r")
+nodes = file.read().split("\n")
+
+question_to_nodes = {}
+
+def get_tree():
+    root = None
+
+    for node in nodes:
+        node = node.replace('"', '')
+        info = node.split(",")
+
+        if(len(info) == 1): continue
+
+        # Is root node
+        if info[1] == "-1":
+            root = DecisionTree(info[0])
+            question_to_nodes[info[0]] = root
+
+        else:
+            parent = question_to_nodes[info[1]]
+            if info[2] == "Yes":
+                parent.insert_yes(info[0])
+                question_to_nodes[info[0]] = parent.yes
+
+            else:
+                parent.insert_no(info[0])
+                question_to_nodes[info[0]] = parent.no
+
+    return root
+
+def preorder(root):
+    #if root is None return
+    if root==None:
+        return
+    #traverse root
+    print(root.data)
+    #traverse left subtree
+    preorder(root.yes)
+    #traverse right subtree
+    preorder(root.no) 
+
